5f3677277bce04ad50b8d74b22e0a867d508a940
YoussefGhollamallah <youssef.gh@live.fr>
2024-01-02 12:08:26 +0100

    job terminé

--- jour01/job02/main.py
+++ jour01/job02/main.py
@@ -1,8 +1,10 @@
 class Operation:
-    nombre1 = 12
-    nombre2 = 3
+    def __init__(self, nombre1: int, nombre2: int):
+        
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
 
-    print("Le nombre1 est : " + str(nombre1))
-    print("Le nombre2 est : " + str(nombre2))
+        print("Le nombre1 est : " + str(nombre1))
+        print("Le nombre2 est : " + str(nombre2))
 
-print(Operation)
+print(Operation(12, 3))
